e5c7adc7b4e4af522ab44ca15612f6b2968a9687
arnavnagpurkar <arnavn.dev@gmail.com>
2024-03-01 17:53:30 +0530

    Implemented logic to shorten a link and send the shortened link to the user

--- actions/handle-link.js
+++ actions/handle-link.js
@@ -1,23 +1,49 @@
 "use server"
+const accessToken = process.env.ACCESS_TOKEN;
 
 export const handleSubmit = async (inputLink) => {
-    const link = inputLink.trim(); // clear the whitespaces
-    // checks whether the link is valid or not
+    const link = inputLink.trim();
     const linkCheck = (link) => {
         if (link) {
             if (!link.includes(" ")) {
                 if (link.startsWith("https://") || link.startsWith("http://")) {
-                    return true;
+                    return true
                 }
             }
             return false;
         }
         return false;
     }
-    // if link is valid then returns true or else returns false
-    const isValid = linkCheck(link);
-    if (isValid) {
-        return true;
+
+    const shortenLink = async (longURL) => {
+        const apiUrl = 'https://api-ssl.bitly.com/v4/shorten';
+        const response = await fetch(apiUrl, {
+            method: "POST",
+            headers: {
+                'Authorization': `Bearer ${accessToken}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                long_url: longURL
+            })
+        })
+        const data = await response.json();
+        const newLink = data.link;
+        return newLink;
     }
-    return false;
+
+    const isValid = linkCheck(link);
+
+    if (isValid) {
+        const shortenedLink = await shortenLink(link);
+        return {
+            isValid,
+            shortenedLink
+        }
+    }
+
+    return {
+        isValid,
+        shortenedLink: "Error: Input link not valid"
+    }
 }
--- app/page.js
+++ app/page.js
@@ -1,17 +1,44 @@
 "use client"
+import React, { useRef, useState } from "react";
+import { handleSubmit } from "../actions/handle-link";
+import Image from "next/image";
 import Link from "next/link";
-import { handleSubmit } from "@/actions/handle-link";
-import { useRef, useState } from "react";
 
 export default function Home() {
   const ref = useRef();
-  const [shortenedLink, setShortenedLink] = useState(null)
+  const [shortenedLink, setShortenedLink] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
 
-  const submitForm = async (e) => {
-    e.preventDefault();
+  const handleGetStarted = () => {
+    const inp = document.querySelector(".link-inp");
+    if (inp) {
+      inp.focus();
+    }
+  };
+
+  const handleRemoveInvalidMsg = () => {
+    setErrorMessage('');
+  }
+
+  const handleLinkCopy = () => {
+    const copyImg = document.querySelector("#copy-img");
+    copyImg.src = "/tick.svg";
+    setTimeout(() => {
+      copyImg.src = "/copy.svg";
+    }, 1500);
+    navigator.clipboard.writeText(shortenedLink)
+  }
+
+  const submitForm = async () => {
     const response = await handleSubmit(ref.current.link.value);
-    console.log(response) // was supposed to be the shortened link, but as project is not completes returns the link is valid or not, will be updated when the logic will be written and then the shortened url will be returned here and then passed to variable "shortenedLink" and the UI changes would be made
-    setShortenedLink(response);
+    if (!response.isValid) {
+      setErrorMessage("Invalid Link");
+      setShortenedLink(null);
+    }
+    else {
+      setShortenedLink(response.shortenedLink);
+      setErrorMessage(null);
+    }
     ref.current.reset();
   };
 
@@ -19,21 +46,82 @@ export default function Home() {
     <>
       <div className="main-heading lg:w-[70%] w-[90%] p-8 mt-14">
         <h1 className="lg:text-7xl text-4xl font-semibold">
-          Shorten All Your Links with <span className="font-extrabold">&quot;iShortener&quot;</span>
+          Shorten All Your Links with{" "}
+          <span className="font-extrabold">&quot;iShortener&quot;</span>
         </h1>
       </div>
       <div className="details lg:w-[70%] w-[90%] p-8 my-5">
-        <p className="text-xl text-gray-300">The best app on the internet to shorten your big links. Get a short link and be productive!</p>
+        <p className="text-xl text-gray-300">
+          The best app on the internet to shorten your big links. Get a short
+          link and be productive!
+        </p>
       </div>
       <div className="getstarted lg:w-[70%] w-[90%] p-6">
-        <Link href="" className="bg-teal-400 hover:bg-teal-500 text-black hover:text-white transition-all text-md font-semibold px-7 py-4 rounded-full">Get Started</Link>
+        <button
+          onClick={handleGetStarted}
+          className="bg-teal-400 hover:bg-teal-500 text-black hover:text-white transition-all text-md font-semibold px-7 py-4 rounded-full"
+        >
+          Get Started
+        </button>
       </div>
-      <div className="shortern-form">
-        <form ref={ref} onSubmit={submitForm} className="lg:w-[85%] w-full py-12 px-10 my-8 flex gap-8 justify-between">
-          <input type="text" placeholder="Enter a link" name="link" id="link" className="text-black px-4 py-5 text-md font-semibold w-full rounded-xl" />
-          <button type="submit" className="lg:min-w-44 min-w-36 bg-teal-400 hover:bg-teal-500 text-black hover:text-white transition-all text-lg font-semibold lg:px-7 px-5 lg:py-4 py-2 rounded-full">Shortern It</button>
+      <div className="shorten-form bg-indigo-700 bg-opacity-65 rounded-xl lg:w-[85%] w-full py-12 px-10 my-8">
+        <form
+          ref={ref}
+          className="flex gap-8 justify-between"
+          onSubmit={(e) => {
+            e.preventDefault();
+            submitForm();
+          }}
+        >
+          <input
+            type="text"
+            placeholder="Enter a link"
+            name="link"
+            id="link"
+            className="link-inp text-black px-4 py-5 text-md font-semibold w-full rounded-xl"
+          />
+          <button
+            type="submit"
+            className="lg:min-w-44 min-w-36 bg-teal-400 hover:bg-teal-500 text-black hover:text-white transition-all text-lg font-semibold lg:px-7 px-5 lg:py-4 py-2 rounded-full"
+          >
+            Shorten It
+          </button>
         </form>
       </div>
+      <div className="message-container">
+        {errorMessage &&
+          <div id="error-msg" className="border-2 rounded-sm border-red-700 bg-red-400 lg:w-[85%] w-full mx-1 my-4 px-5 py-7 font-semibold text-lg flex justify-between">
+            <div>{errorMessage}</div>
+            <button onClick={handleRemoveInvalidMsg}>
+              <Image
+                src="/cancel.svg"
+                width={25}
+                height={25}
+                alt="cancel"
+              />
+            </button>
+          </div>
+        }
+        {shortenedLink &&
+          <div id="shortened-link" className="border-2 rounded-lg border-zinc-700 bg-zinc-700 lg:w-[85%] w-full my-4 px-5 py-7 font-semibold text-lg flex justify-between items-center">
+            <div>
+              <span className="font-normal">Shortened Link :- </span>
+              <Link href={shortenedLink} target="_blank" className="text-blue-500 hover:text-blue-600 transition-all">
+                {shortenedLink}
+              </Link>
+            </div>
+            <button onClick={handleLinkCopy} className="border-2 rounded-2xl border-zinc-700 bg-zinc-800 hover:bg-zinc-600 transition-all p-3">
+              <Image
+                src="/copy.svg"
+                width={25}
+                height={25}
+                alt="cancel"
+                id="copy-img"
+              />
+            </button>
+          </div>
+        }
+      </div >
     </>
   );
 }
