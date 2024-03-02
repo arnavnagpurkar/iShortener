5488a22e7ea0aa4948f64db8250ed4a1c95dc5d9
arnavnagpurkar <arnavn.dev@gmail.com>
2024-03-02 11:59:37 +0530

    Visual Changes, About Page added

--- app/about/page.js
+++ app/about/page.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import Link from "next/link";
 
 const About = () => {
   return (
     <>
-      About Page
+      <div className="py-8 text-zinc-100">
+        <div className="container mx-auto">
+          <div className="max-w-4xl mx-auto text-center mb-8">
+            <h2 className="text-4xl font-bold my-4 mt-0">About iShortener</h2>
+            <p className="text-lg text-gray-400">
+              iShortener is a fast, reliable, and secure URL shortening service designed to make sharing links easier and more efficient.
+            </p>
+          </div>
+          <div className="text-lg">
+            <p className="mx-3 my-4">
+              Whether you're sharing links on social media, sending emails, or communicating through messages, iShortener helps you create concise and manageable URLs that are easy to share and remember.
+            </p>
+            <p className="mx-3 my-4">
+              Our service prioritizes simplicity and speed, allowing you to shorten URLs instantly without any unnecessary steps or registrations. Just paste your link, click a button, and you're ready to share your shortened URL with the world.
+            </p>
+            <p className="mx-3 my-4">
+              We understand the importance of security and privacy, which is why iShortener employs advanced encryption techniques to safeguard your data. You can trust us to protect your information and ensure the integrity of your shortened links.
+            </p>
+            <p className="mx-3 my-4">
+              iShortener is an open-source project created by&nbsp;<Link href="https://github.com/arnavnagpurkar" target="_blank" className="text-blue-500 hover:text-blue-400 transition-all">Arnav Nagpurkar</Link>. It is built using Next.js and Tailwind CSS, leveraging the power of modern web technologies to provide a seamless and efficient URL shortening experience.
+            </p>
+            <p className="mx-3 my-4">
+              You can contribute to its development or report issues on&nbsp;<Link href="https://github.com/arnavnagpurkar/iShortener" target="_blank" className="text-blue-500 hover:text-blue-400 transition-all">GitHub</Link>.
+            </p>
+            <p className='mx-3 my-4'>
+              Thank you for choosing iShortener. We're dedicated to providing you with the best URL shortening experience possible, and we appreciate your support.
+            </p>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
--- components/FeatureCard.js
+++ components/FeatureCard.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const FeatureCard = ({ title, description }) => {
     return (
-        <div className="rounded-lg shadow-md bg-zinc-800 py-7 px-6 text-white w-full hover:scale-105 transition-all duration-300 ease-out">
+        <div className="mx-3 rounded-lg shadow-md bg-zinc-800 py-7 px-6 text-white w-full hover:scale-105 transition-all duration-300 ease-out">
             <h3 className="text-xl font-semibold mb-2">{title}</h3>
             <p className="text-gray-300">{description}</p>
         </div>
